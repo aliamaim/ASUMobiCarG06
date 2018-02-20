1e575755fd68a208437fb28b604a61d1d5e8eee4
SalmaElbehairy <36442599+SalmaElbehairy@users.noreply.github.com>
2018-02-21 00:52:09 +0200

    Add files via upload

---
+++ Arduino Code Examples/Salma Alaa/Problem_2_sheet_1.ino
@@ -0,0 +1,55 @@
+int i =0;
+void setup() 
+{ 
+pinMode ( 4,OUTPUT);
+pinMode ( 5,OUTPUT);
+pinMode ( 6,OUTPUT);
+pinMode ( 7,OUTPUT);
+}
+
+void loop()
+{
+  if (i==0)
+  { 
+  digitalWrite(4, HIGH);  
+  delay(1000);                      
+  digitalWrite(4, LOW);  
+  delay(1000); 
+  
+  digitalWrite(5, HIGH);   
+  delay(1000);                      
+  digitalWrite(5, LOW);    
+  delay(1000);  
+  
+   digitalWrite(6, HIGH);   
+  delay(1000);                     
+  digitalWrite(6, LOW);   
+  delay(1000); 
+   
+   digitalWrite(7, HIGH);   
+  delay(1000);                      
+  digitalWrite(7, LOW);    
+  delay(1000);
+    digitalWrite(7, HIGH);  
+  delay(1000);                      
+  digitalWrite(7, LOW);  
+  delay(1000); 
+  
+  digitalWrite(6, HIGH);   
+  delay(1000);                      
+  digitalWrite(6, LOW);    
+  delay(1000);
+  
+  digitalWrite(5, HIGH);   
+  delay(1000);                     
+  digitalWrite(5, LOW);   
+  delay(1000);  
+  
+  digitalWrite(4, HIGH);   
+  delay(1000);                      
+  digitalWrite(4, LOW);    
+  delay(1000); 
+  }
+ 
+  
+}
---
+++ Arduino Code Examples/Salma Alaa/problem_1_sheet_1.ino
@@ -0,0 +1,12 @@
+void setup() {
+ 
+pinMode(5, OUTPUT);
+}
+
+void loop() {
+ 
+digitalWrite(5, HIGH);  
+  delay(2000);                      
+  digitalWrite(5, LOW);   
+    delay(6000);  
+}
